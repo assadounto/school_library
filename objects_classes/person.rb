a1290e7236e28daed45554945f9c0460bde2c3ec
rich <adukyerer@gmail.com>
2022-08-22 16:08:29 +0000

    fix last linter errors

--- objects_classes/person.rb
+++ objects_classes/person.rb
@@ -1,22 +1,22 @@
 class Person
   attr_accessor :name, :age, :id, :parent_permission
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @name = name
     @age = age
     @id = Random.rand(1..100)
     @parent_permission = parent_permission
   end
 
-  def is_of_age?
-    @age >= 18 ? true : false
-    end
+  def of_age?
+    @age >= 18
+  end
 
   def can_use_services?
-     @parent_permission || is_of_age? ? true : false
-    end
+    @parent_permission || is_of_age?
+  end
 end
 
 person = Person.new(2, 'rich')
-puts person.is_of_age?
+puts person.of_age?
 p person.can_use_services?
