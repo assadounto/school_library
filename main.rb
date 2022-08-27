b9f00103dbe4cab303bc40ffbe7b9bb0431b8c70
rich <adukyerer@gmail.com>
2022-08-27 17:17:55 +0000

    make changes to cli order

--- main.rb
+++ main.rb
@@ -28,9 +28,9 @@ class Main
     @reply = gets.chomp.to_i
     case @reply
     when 1
-      @app.list_people
-    when 2
       @app.list_books
+    when 2
+      @app.list_people
     when 3
       @app.create_person
     when 4
